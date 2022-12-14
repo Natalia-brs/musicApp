3d1d052bc1f7c8de91b6be852eef5fdb83a417f5
natalia <ntla.brs@gmail.com>
2022-12-14 16:12:56 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,54 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
+
+  handleArtist = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  validateArtist = () => {
+    const numbervalid = 2;
+    const { name } = this.state;
+    const valid = (name.length < numbervalid);
+    return valid;
+  };
+
   render() {
+    const { name } = this.state;
     return (
       <div data-testid="page-search">
-        Search
+        <form>
+          <label htmlFor="find">
+            <span>Artista:</span>
+            <input
+              placeholder="Nome do Artista"
+              type="text"
+              data-testid="search-artist-input"
+              id="find"
+              name="name"
+              value={ name }
+              onChange={ this.handleArtist }
+            />
+          </label>
+
+          <button
+            disabled={ this.validateArtist() }
+            type="button"
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+
+          </button>
+        </form>
         <Header />
       </div>
     );
