301926de5d9c33c373ea29450adc429cf03939ff
natalia <ntla.brs@gmail.com>
2022-12-19 10:42:25 -0300

    Refatorando e add requisito 9 e 11, finaliza projeto

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Loading from './Loading';
 
 class MusicCard extends Component {
   constructor() {
@@ -8,67 +7,63 @@ class MusicCard extends Component {
 
     this.state = {
       favoritesInput: false,
-      verifyLoad: false,
     };
   }
 
+  // apos a renderização é velicado se a musica ja é favorita
   componentDidMount() {
-    this.changeFavorite();
-  }
-
-  changeFavorite = () => {
     const { favorite } = this.props;
     this.setState({
       favoritesInput: favorite,
     });
-  };
+  }
 
-  handleChecked = ({ target }) => {
-    const { name } = target;
+  // o input ao receber um click, é atualizado se o mesmo esta checked ou não;
+  // atualiza o estado da musica favorita conforme o checked
+  handleChecked = ({ target: { checked } }) => {
     this.setState({
-      [name]: target.checked,
+      favoritesInput: checked,
     });
   };
 
   render() {
-    const { favoritesInput, verifyLoad } = this.state;
     const {
       addFavorite,
       trackId,
       previewUrl,
       trackName } = this.props;
+
+    const { favoritesInput } = this.state;
+
     return (
-
       <div>
-        {
-          verifyLoad && <Loading />
-        }
-        <span>
-          {' '}
-          { trackName }
-          {' '}
-        </span>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          {' '}
-          <code>audio</code>
-          .
-        </audio>
-
-        <label htmlFor="favorites">
-          Favorita
-          <input
-            data-testid={ `checkbox-music-${trackId}` }
-            type="checkbox"
-            id="favorites"
-            value={ favoritesInput }
-            name={ trackId }
-            onChange={ addFavorite }
-            onClick={ this.handleChecked }
-          />
-        </label>
+        <div>
+          <h4>{ trackName }</h4>
+          <audio data-testid="audio-component" src={ previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            {' '}
+            {' '}
+            <code>audio</code>
+            .
+          </audio>
+          <label htmlFor="favorites">
+            Favorita
+            <input
+              data-testid={ `checkbox-music-${trackId}` }
+              type="checkbox"
+              id="favorites"
+              // true or false, dependendo do valor armazenado no estado co componente
+              checked={ favoritesInput }
+              // name ->  armazena o id da musica que será utilizado na função addFavorite do componente Album
+              name={ trackId }
+              // addFavorite -> recebe a função addFavorite do componente Album responsável por adicionar ou remover musica favorita
+              onChange={ addFavorite }
+              // atualiza o estado da musica favorita conforme o checked
+              onClick={ this.handleChecked }
+            />
+          </label>
+        </div>
       </div>
     );
   }
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -4,7 +4,11 @@ import Header from '../components/Header';
 import Loading from '../components/Loading';
 import MusicCard from '../components/MusicCard';
 import getMusics from '../services/musicsAPI';
-import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+import {
+  addSong,
+  getFavoriteSongs,
+  removeSong,
+} from '../services/favoriteSongsAPI';
 
 class Album extends Component {
   constructor() {
@@ -19,103 +23,135 @@ class Album extends Component {
     };
   }
 
+  //  após a renderização do componente é feito o carregamento das musicas do album e das musicas favoritas
   componentDidMount() {
     this.loadMusicList();
     this.fetchFavorites();
   }
 
-  loadMusicList = () => {
+  // obtém o id do album através do parâmetro da url
+  getIdFromURL = async () => {
     const { match } = this.props;
     const { params: { id } } = match;
-    this.setState({
-      verifyLoad: true,
-    }, async () => {
-      const musicList = await getMusics(id);
-      this.setState({
-        songsTrack: musicList,
-        verifyLoad: false,
-      });
-    });
+    return id;
   };
 
+  // efetua a requisição das musicas através da função getMusics
+  loadMusicList = async () => {
+    this.setState(
+      { verifyLoad: true },
+      async () => {
+        const id = await this.getIdFromURL();
+        const musicList = await getMusics(id);
+        this.setState({
+          songsTrack: musicList,
+          verifyLoad: false,
+        });
+      },
+    );
+  };
+
+  // efetua o carregamento das musicas favoritas e salva no estado do componente
   fetchFavorites = async () => {
-    this.setState({
-      catchFavorites: true,
-    }, async () => {
-      const getFav = await getFavoriteSongs();
-      console.log(getFav);
-      this.setState({
-        favoriteSongs: getFav,
-        catchFavorites: false,
-      });
-    });
+    this.setState(
+      { catchFavorites: true },
+      async () => {
+        const getFav = await getFavoriteSongs();
+        this.setState({
+          catchFavorites: false,
+          favoriteSongs: getFav,
+        });
+      },
+    );
   };
 
+  // obtém o id através do name do input
+  // verifica se o checkbox está checked (adiciona ou remove a musica das favoritas)
   addFavorite = async ({ target }) => {
     const { songsTrack } = this.state;
-    const [, ...list] = songsTrack;
-    const track = list.find((song) => song.trackId === Number(target.name));
-    const check = target.checked;
+    const list = songsTrack.slice(1);
 
-    if (check) {
-      this.setState({
-        newLoad: true,
-      }, async () => {
-        await addSong(track);
-        this.setState({
-          newLoad: false,
-        });
-      });
+    // encontra a musica de acordo com id passado pelo target.name
+    // track armazena um objeto com as informações da musica o qual é usado para adicionar(addSong) ou remover(removeSong)
+    const track = list.find((song) => song.trackId === Number(target.name));
+
+    if (target.checked) {
+      this.setState(
+        {
+          newLoad: true,
+        },
+        async () => {
+          await addSong(track);
+          this.setState({
+            newLoad: false,
+          });
+        },
+      );
     } else {
-      this.setState({
-        newLoad: true,
-      }, async () => {
-        await removeSong(track);
-        this.setState({
-          newLoad: false,
-        });
-      });
+      this.setState(
+        {
+          newLoad: true,
+        },
+        async () => {
+          await removeSong(track);
+          this.setState({
+            newLoad: false,
+          });
+        },
+      );
     }
   };
 
   render() {
     const { songsTrack, verifyLoad, newLoad, catchFavorites, favoriteSongs } = this.state;
     const [albumDetails, ...listTracks] = songsTrack;
+
+    /* controla o loading das musicas favoritas através da função  fetchFavorites */
+    if (catchFavorites) return <Loading />;
+
     return (
       <div data-testid="page-album">
+
         <Header />
-        {
-          verifyLoad && <Loading />
-        }
-        {
-          catchFavorites && <Loading />
-        }
+
+        {/* controla o loading das musicas obtidas pela api através da função loadMusicList */}
+        {verifyLoad && <Loading />}
+
         <div>
-          { albumDetails && (
+          {
+            albumDetails
+          && (
             <div>
               <span data-testid="artist-name">
                 Artista:
-                { albumDetails.artistName}
+                {albumDetails.artistName}
                 {' '}
               </span>
               <span data-testid="album-name">
                 Album:
-                {albumDetails.collectionName }
+                {albumDetails.collectionName}
                 {' '}
               </span>
-            </div>
-          )}
 
+              {/* controla o loading quando adiciona ou remove uma musica favorita através das função addFavorite  */}
+              {newLoad && <Loading />}
+
+              {listTracks.map((tracks) => (
+                <MusicCard
+                  { ...tracks }
+                  key={ tracks.trackId }
+                  // favorite -> props armazena um boll, true caso a mesma ja tenha sido favoritada, será utilizada no componente MusicCard
+                  favorite={ favoriteSongs.some(
+                    (music) => music.trackId === tracks.trackId,
+                  ) }
+                  // função que sera utilizada no onChange no componente MusicCard
+                  addFavorite={ this.addFavorite }
+                />
+              ))}
+            </div>)
+          }
         </div>
-        {
-          newLoad && <Loading />
-        }
-        {listTracks.map((tracks) => (<MusicCard
-          { ...tracks }
-          key={ tracks.trackId }
-          favorite={ favoriteSongs.some((song) => song.trackId === tracks.trackId) }
-          addFavorite={ this.addFavorite }
-        />)) }
+
       </div>
     );
   }
